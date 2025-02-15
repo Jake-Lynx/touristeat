1483b60629da8685a224f43d1c54623f8b3e3439
Jake Lynx <jake.lynx.7@gmail.com>
2025-02-15 01:48:07 +0000

    Fix middleware bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,24 +8,33 @@ const CACHE_DURATION = 60 * 1000 * 10; // 10 minutes
 
 export async function middleware(req: NextRequest) {
 
+  // Check session token and token
   const sessionToken = req.cookies.get('next-auth.session-token')?.value;
   
   if (!sessionToken) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (token) {
+      tokenCache.set('temp-token', {
+        token,
+        timestamp: Date.now()
+      })
+      return NextResponse.next()
+    }
     return NextResponse.redirect(new URL('/admin', req.url));
   }
-
-  // Check cache
+  
+  // If we've a session token, check cache
   const cachedResult = tokenCache.get(sessionToken);
   if (cachedResult && Date.now() - cachedResult.timestamp < CACHE_DURATION) {
     return cachedResult.token
-      ? NextResponse.next()
-      : NextResponse.redirect(new URL('/admin', req.url)) 
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL('/admin', req.url)) 
   }
-
-  // If no cache, check token
+  
+  // If we don't have a session token or it's expired, check token
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  // Cache the result
+  // Update cache
   tokenCache.set(
     sessionToken,
     {
@@ -34,12 +43,9 @@ export async function middleware(req: NextRequest) {
     }
   )
 
-  // Maintain the user on the admin page if they are not connected
-  if (!token) {
-    return NextResponse.redirect(new URL('/admin', req.url));
-  }
-
-  return NextResponse.next();
+  return token
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL('/admin', req.url))
 }
 
 // Routes sur lesquelles le middleware s'applique
